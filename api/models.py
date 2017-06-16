f4e85f97c6e7442e155e53c219c9429424f8e646
Bruno Melo <brunousml@gmail.com>
2017-06-15 21:47:44 -0300

    remove unsed code in models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,4 @@
-from django.db import models
-
-# Create your models here.# This is an auto-generated Django model module.
+# This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
