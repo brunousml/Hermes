b4afa18fff0ef2d1cf6518b1b0f9d4a1d7c3af96
Bruno Melo <brunousml@gmail.com>
2017-06-18 00:30:37 -0300

    version 1 api.

--- Hermes/urls.py
+++ Hermes/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from tastypie.api import Api
 from api.resources import CouncilmanDebitsResource
 
-councilmanDebits_resource = CouncilmanDebitsResource()
+v1_api = Api(api_name='v1')
+v1_api.register(CouncilmanDebitsResource())
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(councilmanDebits_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 ]
