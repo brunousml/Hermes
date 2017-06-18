dff9045a639d4fdcf0f51b9b0abb81caddf037e4
Bruno Melo <brunousml@gmail.com>
2017-06-18 00:30:47 -0300

    add councilman resource

--- api/resources.py
+++ api/resources.py
@@ -1,9 +1,17 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
-from api.models import CouncilmanDebits
+from api.models import CouncilmanDebits, Councilman
+
+
+class CouncilmanResource(ModelResource):
+    class Meta:
+        queryset = Councilman.objects.all()
+        resource_name = 'Councilman'
 
 
 class CouncilmanDebitsResource(ModelResource):
+    councilman = fields.ToOneField(CouncilmanResource, 'councilman', full=True)
+
     class Meta:
         queryset = CouncilmanDebits.objects.all()
         resource_name = 'CouncilmanDebits'
-
